794a077752db2623a25288e5a406de7939981a06
mwxis <101027285+mwxis@users.noreply.github.com>
2022-03-14 13:34:43 -0400

    First version of code

---
+++ numberFun.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class numberFun {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Num 1: ");
+        num1 =input.nextInt();
+        System.out.print("Num 2: ");
+        num2 =input.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+
+    }
+}
